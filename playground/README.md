51155068aeec6424bb3c02b9157f0a3bb0f9370b
David L. Qiu <david@qiu.dev>
2023-02-20 21:32:07 +0000

    add playground directory

---
+++ playground/README.md
@@ -0,0 +1,5 @@
+# jupyter-ai playground
+
+Contents root that should be served by JupyterLab when developing Jupyter AI. To
+use the GPT-3 and DALL-E model engines, an example configuration is provided in
+`config.py`.
---
+++ playground/config.example.py
@@ -0,0 +1,7 @@
+# JupyterLab configuration file for Jupyter AI local development
+# Reference: https://jupyter-server.readthedocs.io/en/latest/other/full-config.html
+
+# c.GPT3ModelEngine.api_key  = "<YOUR-API-KEY-HERE>"
+# c.DalleModelEngine.api_key = "<YOUR-API-KEY-HERE>"
+
+asdf
