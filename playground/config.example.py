16bb2447237a69f6e23fa30b7764d8640cec9579
Piyush Jain <piyushjain@duck.com>
2023-02-21 14:11:15 -0800

    Fixed build dependencies in default shell

--- playground/config.example.py
+++ playground/config.example.py
@@ -1,7 +1,9 @@
 # JupyterLab configuration file for Jupyter AI local development
 # Reference: https://jupyter-server.readthedocs.io/en/latest/other/full-config.html
 
-# c.GPT3ModelEngine.api_key  = "<YOUR-API-KEY-HERE>"
-# c.DalleModelEngine.api_key = "<YOUR-API-KEY-HERE>"
+#c.GPT3ModelEngine.api_key  = "<YOUR-API-KEY-HERE>"
+#c.DalleModelEngine.api_key = "<YOUR-API-KEY-HERE>"
 
-asdf
+# Specify full path to the notebook dir if running jupyter lab from
+# outside of the jupyter-ai project root directory 
+c["notebook-dir"] = "./playground"
