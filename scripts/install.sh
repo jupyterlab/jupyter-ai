39f651c1df438a2cd754e1df59f0fb7048b4c014
David L. Qiu <david@qiu.dev>
2023-03-07 16:39:48 +0000

    fix dev install script

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,3 +1,9 @@
-for dir in packages/* 
-    do (cd "$dir" && pip install -e ".[test]") || exit 1
+for dir in packages/*; do
+    if [ $dir = "packages/jupyter-ai-module-cookiecutter" ]; then
+        echo "skipped"
+        continue
+    fi
+
+    echo "Installing $dir in editable mode..."
+    (cd "$dir" && pip install -e ".[test]") || exit 1
 done
