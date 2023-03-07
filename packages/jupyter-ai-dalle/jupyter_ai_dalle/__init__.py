af4c214570dd8eea2af2e8ff5a5b0ad9029ba96c
David L. Qiu <david@qiu.dev>
2023-03-07 16:43:58 +0000

    finish renaming remaining occurrences of TestModelEngine

--- packages/jupyter-ai-dalle/jupyter_ai_dalle/__init__.py
+++ packages/jupyter-ai-dalle/jupyter_ai_dalle/__init__.py
@@ -1,6 +1,6 @@
 from ._version import __version__
 
-# expose TestModelEngine on the root module so that it may be declared as an
+# expose DalleModelEngine on the root module so that it may be declared as an
 # entrypoint in `pyproject.toml`
 from .engine import DalleModelEngine
 
