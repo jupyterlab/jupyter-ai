8ab5dcecbd05c425442043c78be3487e67cf1a8d
Jason Weill <jweill@amazon.com>
2023-04-06 15:36:48 -0700

    More detailed traceback

--- packages/jupyter-ai/jupyter_ai/exception.py
+++ packages/jupyter-ai/jupyter_ai/exception.py
@@ -1,9 +1,11 @@
 from IPython.core.magic import register_line_magic
 
+import traceback
+
 def store_exception(shell, etype, evalue, tb, tb_offset=None):
     prompt_number = shell.execution_count
     err = shell.user_ns.get("Err", {})
-    err[prompt_number] = str(evalue)
+    err[prompt_number] = str(etype) + str(evalue) + str(traceback.format_tb(tb))
     shell.user_ns["Err"] = err
     # Rereaise the exception
     raise
