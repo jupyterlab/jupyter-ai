304f7875e480832eac02a8518eb2e8ba60b0b1a6
Jason Weill <jweill@amazon.com>
2023-04-07 10:21:15 -0700

    Uses shell.showtraceback

--- packages/jupyter-ai/jupyter_ai/exception.py
+++ packages/jupyter-ai/jupyter_ai/exception.py
@@ -1,4 +1,3 @@
-from IPython.core import ultratb
 from IPython.core.magic import register_line_magic
 from IPython.core.getipython import get_ipython
 
@@ -6,13 +5,9 @@ import traceback
 
 def store_exception(shell, etype, evalue, tb, tb_offset=None):
     # Create a plain text traceback formatter
-    tb_formatter = ultratb.FormattedTB(color_scheme='NoColor')
-    tb_formatter.set_mode('Verbose')
-    styled_exception = '\n'.join(tb_formatter.structured_traceback(etype, evalue, tb, tb_offset))
+    styled_exception = shell.showtraceback()
 
     prompt_number = shell.execution_count
     err = shell.user_ns.get("Err", {})
     err[prompt_number] = styled_exception
     shell.user_ns["Err"] = err
-    # Rereaise the exception
-    raise
