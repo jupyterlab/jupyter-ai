5fa34c47ba6305d1c8bee94cb779b2f29fe0d600
Jason Weill <jweill@amazon.com>
2023-04-07 17:34:39 -0700

    Displays traceback info from tb, needs to return a structured traceback

--- packages/jupyter-ai/jupyter_ai/exception.py
+++ packages/jupyter-ai/jupyter_ai/exception.py
@@ -1,13 +1,24 @@
 from IPython.core.magic import register_line_magic
 from IPython.core.getipython import get_ipython
+from IPython.core.ultratb import ListTB
 
 import traceback
 
 def store_exception(shell, etype, evalue, tb, tb_offset=None):
-    # Create a plain text traceback formatter
-    styled_exception = shell.showtraceback()
+    # Model the exception in plain text
+    if tb:
+        estring = '\n'.join(traceback.extract_tb(tb).format())
+    else:
+        estring = None
+
+    styled_exception = ('Exception type: ' + str(etype.__name__) +
+        '\nException value: ' + str(evalue) +
+        '\nTraceback: ' + estring)
 
     prompt_number = shell.execution_count
     err = shell.user_ns.get("Err", {})
     err[prompt_number] = styled_exception
     shell.user_ns["Err"] = err
+    
+    # TODO: Return structured traceback
+    return None
