d4b81e0539a26a74109e094d3df029279f518222
David L. Qiu <david@qiu.dev>
2023-04-10 20:41:08 +0000

    remove scripts/install.sh

--- scripts/install.sh
+++
@@ -1,9 +0,0 @@
-for dir in packages/*; do
-    if [ $dir = "packages/jupyter-ai-module-cookiecutter" ]; then
-        echo "skipped"
-        continue
-    fi
-
-    echo "Installing $dir in editable mode..."
-    (cd "$dir" && pip install -e ".[test]") || exit 1
-done
