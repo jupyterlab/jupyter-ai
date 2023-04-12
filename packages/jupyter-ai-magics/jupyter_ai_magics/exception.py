e62109d1501457794f5e7940b3de902415caecff
Jason Weill <jweill@amazon.com>
2023-04-11 18:04:37 -0700

    Disables ANSI color strings so that the error is captured in text

--- packages/jupyter-ai-magics/jupyter_ai_magics/exception.py
+++ packages/jupyter-ai-magics/jupyter_ai_magics/exception.py
@@ -6,8 +6,14 @@ import traceback
 
 def store_exception(shell, etype, evalue, tb, tb_offset=None):
     # A structured traceback (a list of strings) or None
+    # Disable ANSI color strings
+    shell.InteractiveTB.color_toggle()
+
     stb = shell.InteractiveTB.structured_traceback(etype, evalue, tb, tb_offset=tb_offset)
     stb_text = shell.InteractiveTB.stb2text(stb)
+
+    # Re-enable ANSI color strings
+    shell.InteractiveTB.color_toggle()
 
     etraceback = shell.showtraceback()
 
