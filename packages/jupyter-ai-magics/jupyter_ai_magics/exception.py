117b6c259c19f3e1f270209b699e3e8f497674fb
Jason Weill <jweill@amazon.com>
2023-04-11 18:23:51 -0700

    Handles syntax errors

--- packages/jupyter-ai-magics/jupyter_ai_magics/exception.py
+++ packages/jupyter-ai-magics/jupyter_ai_magics/exception.py
@@ -6,14 +6,22 @@ import traceback
 
 def store_exception(shell, etype, evalue, tb, tb_offset=None):
     # A structured traceback (a list of strings) or None
-    # Disable ANSI color strings
-    shell.InteractiveTB.color_toggle()
-
-    stb = shell.InteractiveTB.structured_traceback(etype, evalue, tb, tb_offset=tb_offset)
-    stb_text = shell.InteractiveTB.stb2text(stb)
-
-    # Re-enable ANSI color strings
-    shell.InteractiveTB.color_toggle()
+ 
+    if issubclass(etype, SyntaxError):
+        # Disable ANSI color strings
+        shell.SyntaxTB.color_toggle()
+        # Don't display a stacktrace because a syntax error has no stacktrace
+        stb = shell.SyntaxTB.structured_traceback(etype, evalue, [])
+        stb_text = shell.SyntaxTB.stb2text(stb)
+        # Re-enable ANSI color strings
+        shell.SyntaxTB.color_toggle()
+    else:
+        # Disable ANSI color strings
+        shell.InteractiveTB.color_toggle()
+        stb = shell.InteractiveTB.structured_traceback(etype, evalue, tb, tb_offset=tb_offset)
+        stb_text = shell.InteractiveTB.stb2text(stb)
+        # Re-enable ANSI color strings
+        shell.InteractiveTB.color_toggle()
 
     etraceback = shell.showtraceback()
 
