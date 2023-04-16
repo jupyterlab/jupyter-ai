bd1842bdeded79740083c3f95de97631695c7a97
Jason Weill <jweill@amazon.com>
2023-04-16 09:59:20 -0700

    Reinstates cookiecutter

---
+++ packages/jupyter-ai-module-cookiecutter/{{cookiecutter.labextension_name}}/src/index.ts
@@ -0,0 +1,41 @@
+import {
+  JupyterFrontEnd,
+  JupyterFrontEndPlugin
+} from '@jupyterlab/application';
+
+import type { InsertionContext } from '@jupyter-ai/core';{% if cookiecutter.has_settings.lower().startswith('y') %}
+
+import { ISettingRegistry } from '@jupyterlab/settingregistry';{% endif %}
+
+/**
+ * Initialization data for the {{ cookiecutter.labextension_name }} extension.
+ */
+const plugin: JupyterFrontEndPlugin<void> = {
+  id: '{{ cookiecutter.labextension_name }}:plugin',
+  autoStart: true,{% if cookiecutter.has_settings.lower().startswith('y') %}
+  optional: [ISettingRegistry],{% endif %}
+  activate: (app: JupyterFrontEnd{% if cookiecutter.has_settings.lower().startswith('y') %}, settingRegistry: ISettingRegistry | null{% endif %}) => {
+    console.log('JupyterLab extension {{ cookiecutter.labextension_name }} is activated!');{% if cookiecutter.has_settings.lower().startswith('y') %}
+
+    if (settingRegistry) {
+      settingRegistry
+        .load(plugin.id)
+        .then(settings => {
+          console.log('{{ cookiecutter.labextension_name }} settings loaded:', settings.composite);
+        })
+        .catch(reason => {
+          console.error('Failed to load settings for {{ cookiecutter.labextension_name }}.', reason);
+        });
+    }{% endif %}
+
+    // handles "test" insertion mode, which just shows output in a native
+    // browser alert.
+    app.commands.addCommand('ai:insert-test', {
+      execute: ((context: InsertionContext) => {
+        alert(context.response.output)
+      }) as any
+    })
+  }
+};
+
+export default plugin;
