ca17ea2ba28d02cd5cdb9ac52ab2f2a41f2a1b0f
Jason Weill <93281816+JasonWeill@users.noreply.github.com>
2023-04-16 10:15:46 -0700

    Removes chatgpt, dalle modules (#71)
    * Removes chatgpt, dalle modules, cookiecutter
    
    * Reinstates cookiecutter

--- packages/jupyter-ai-dalle/src/__tests__/jupyter_ai_dalle.spec.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Example of [Jest](https://jestjs.io/docs/getting-started) unit tests
- */
-
-describe('jupyter_ai_dalle', () => {
-  it('should be tested', () => {
-    expect(1 + 1).toEqual(2);
-  });
-});
