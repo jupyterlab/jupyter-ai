fbc8e9736a8ca7137e018b28aafc17a189eee392
David L. Qiu <david@qiu.dev>
2023-05-26 21:39:29 +0000

    update dev install script to install doc dependencies

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
-pip install jupyterlab
+# install core packages
+pip install jupyterlab~=3.0
 cp playground/config.example.py playground/config.py
 jlpm install
 jlpm dev-install
 
+# install docs packages
+pip install sphinx
+pip install -r docs/requirements.txt
+
+# install grpcio via conda for ARM platforms to preserve compatibility with Ray
 if [[ $(uname -m) == 'arm64' ]]; then
     pip uninstall grpcio && conda install -y grpcio
 fi
