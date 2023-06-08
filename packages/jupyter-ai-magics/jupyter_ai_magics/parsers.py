83c45eb1c9604f52e85c172aadec1c12b49d30e6
Jason Weill <93281816+JasonWeill@users.noreply.github.com>
2023-06-08 16:57:37 -0700

    SageMaker endpoint magic command support (#215)
    * WIP: New parameters for SageMaker Endpoint
    
    * Uses symbolic constants consistently
    
    * Updates sample notebook
    
    * Updates docs, sample notebook
    
    * Retitles section to be about magic commands
    
    * Removes AWS_SESSION_TOKEN
    
    * Links for more info
    
    * Update docs/source/users/index.md
    
    Co-authored-by: Piyush Jain <piyushjain@duck.com>
    
    * Additional copy edits per @3coins
    
    * Update docs/source/users/index.md
    
    ---------
    
    Co-authored-by: Piyush Jain <piyushjain@duck.com>

--- packages/jupyter-ai-magics/jupyter_ai_magics/parsers.py
+++ packages/jupyter-ai-magics/jupyter_ai_magics/parsers.py
@@ -6,17 +6,42 @@ FORMAT_CHOICES_TYPE = Literal["code", "html", "image", "json", "markdown", "math
 FORMAT_CHOICES = list(get_args(FORMAT_CHOICES_TYPE))
 FORMAT_HELP = """IPython display to use when rendering output. [default="markdown"]"""
 
+REGION_NAME_SHORT_OPTION = '-n'
+REGION_NAME_LONG_OPTION = '--region-name'
+REGION_NAME_HELP = ("AWS region name, e.g. 'us-east-1'. Required for SageMaker provider; " +
+    "does nothing with other providers.")
+
+REQUEST_SCHEMA_SHORT_OPTION = '-q'
+REQUEST_SCHEMA_LONG_OPTION = '--request-schema'
+REQUEST_SCHEMA_HELP = ("The JSON object the endpoint expects, with the prompt being " +
+    "substituted into any value that matches the string literal '<prompt>'. " +
+    "Required for SageMaker provider; does nothing with other providers.")
+
+RESPONSE_PATH_SHORT_OPTION = '-p'
+RESPONSE_PATH_LONG_OPTION = '--response-path'
+RESPONSE_PATH_HELP = ("A JSONPath string that retrieves the language model's output " +
+    "from the endpoint's JSON response. Required for SageMaker provider; " +
+    "does nothing with other providers.")
+
 class CellArgs(BaseModel):
     type: Literal["root"] = "root"
     model_id: str
     format: FORMAT_CHOICES_TYPE
     reset: bool
+    # The following parameters are required only for SageMaker models
+    region_name: Optional[str]
+    request_schema: Optional[str]
+    response_path: Optional[str]
 
 # Should match CellArgs, but without "reset"
 class ErrorArgs(BaseModel):
     type: Literal["error"] = "error"
     model_id: str
     format: FORMAT_CHOICES_TYPE
+    # The following parameters are required only for SageMaker models
+    region_name: Optional[str]
+    request_schema: Optional[str]
+    response_path: Optional[str]
 
 class HelpArgs(BaseModel):
     type: Literal["help"] = "help"
@@ -60,6 +85,9 @@ class LineMagicGroup(click.Group):
     help="""Clears the conversation transcript used when interacting with an
     OpenAI chat model provider. Does nothing with other providers."""
 )
+@click.option(REGION_NAME_SHORT_OPTION, REGION_NAME_LONG_OPTION, required=False, help=REGION_NAME_HELP)
+@click.option(REQUEST_SCHEMA_SHORT_OPTION, REQUEST_SCHEMA_LONG_OPTION, required=False, help=REQUEST_SCHEMA_HELP)
+@click.option(RESPONSE_PATH_SHORT_OPTION, RESPONSE_PATH_LONG_OPTION, required=False, help=RESPONSE_PATH_HELP)
 def cell_magic_parser(**kwargs):
     """
     Invokes a language model identified by MODEL_ID, with the prompt being
@@ -84,6 +112,9 @@ def line_magic_parser():
     default="markdown",
     help=FORMAT_HELP
 )
+@click.option(REGION_NAME_SHORT_OPTION, REGION_NAME_LONG_OPTION, required=False, help=REGION_NAME_HELP)
+@click.option(REQUEST_SCHEMA_SHORT_OPTION, REQUEST_SCHEMA_LONG_OPTION, required=False, help=REQUEST_SCHEMA_HELP)
+@click.option(RESPONSE_PATH_SHORT_OPTION, RESPONSE_PATH_LONG_OPTION, required=False, help=RESPONSE_PATH_HELP)
 def error_subparser(**kwargs):
     """
     Explains the most recent error. Takes the same options (except -r) as
