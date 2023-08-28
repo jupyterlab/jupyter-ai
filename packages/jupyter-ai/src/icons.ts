771c52aec97e22036bcef82d2eb21f7e87ff9870
ran <redamancy0326@gmail.com>
2023-08-28 14:10:41 +0800

    update bigcode icon

--- packages/jupyter-ai/src/icons.ts
+++ packages/jupyter-ai/src/icons.ts
@@ -4,6 +4,7 @@ import { LabIcon } from '@jupyterlab/ui-components';
 
 import chatSvgStr from '../style/icons/chat.svg';
 import jupyternautSvg from '../style/icons/jupyternaut.svg';
+import bigcodeSvg from '../style/icons/bigcode.svg';
 
 export const chatIcon = new LabIcon({
   name: 'jupyter-ai::chat',
@@ -13,6 +14,11 @@ export const chatIcon = new LabIcon({
 export const jupyternautIcon = new LabIcon({
   name: 'jupyter-ai::jupyternaut',
   svgstr: jupyternautSvg
+});
+
+export const bigCodeIcon = new LabIcon({
+  name: 'jupyter-ai::code-completion-settings',
+  svgstr: bigcodeSvg
 });
 
 export const Jupyternaut = jupyternautIcon.react;
