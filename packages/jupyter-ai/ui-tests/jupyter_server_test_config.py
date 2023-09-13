9763054cff8f6b9249a59249488e08e92c05d63a
Andrii Ieroshenko <ieroshenkoa@gmail.com>
2023-09-12 23:57:39 -0700

    update server test config

--- packages/jupyter-ai/ui-tests/jupyter_server_test_config.py
+++ packages/jupyter-ai/ui-tests/jupyter_server_test_config.py
@@ -4,9 +4,17 @@
 opens the server to the world and provide access to JupyterLab
 JavaScript objects through the global window variable.
 """
-from jupyterlab.galata import configure_jupyter_server
+from tempfile import mkdtemp
 
-configure_jupyter_server(c)
+c.ServerApp.port = 8888
+c.ServerApp.port_retries = 0
+c.ServerApp.open_browser = False
+
+c.ServerApp.root_dir = mkdtemp(prefix="galata-test-")
+c.ServerApp.token = ""
+c.ServerApp.password = ""
+c.ServerApp.disable_check_xsrf = True
+c.LabApp.expose_app_in_browser = True
 
 # Uncomment to set server log level to debug level
 # c.ServerApp.log_level = "DEBUG"
