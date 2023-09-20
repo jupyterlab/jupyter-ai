53f1b567a9751430134e336e756fc0f64b2a45ed
Nazar Volosetskyy <nazar.volosetskyy@syapse.com>
2023-09-20 17:06:23 -0400

    jupyter_ai_magics-2.2.17 and patientId==patientid

--- packages/jupyter-ai-magics/jupyter_ai_magics/utils.py
+++ packages/jupyter-ai-magics/jupyter_ai_magics/utils.py
@@ -7,12 +7,90 @@ from jupyter_ai_magics.aliases import MODEL_ID_ALIASES
 from jupyter_ai_magics.embedding_providers import BaseEmbeddingsProvider
 from jupyter_ai_magics.providers import BaseProvider
 
+import plotly.express as px
+import plotly.graph_objects as go
+import pandas as pd
+
+
 Logger = Union[logging.Logger, logging.LoggerAdapter]
 LmProvidersDict = Dict[str, BaseProvider]
 EmProvidersDict = Dict[str, BaseEmbeddingsProvider]
 AnyProvider = Union[BaseProvider, BaseEmbeddingsProvider]
 ProviderDict = Dict[str, AnyProvider]
 
+
+def genSankey(df,cat_cols=[],value_cols='',title='Sankey Diagram'):
+    # maximum of 6 value cols -> 6 colors
+    colorPalette = ['#4B8BBE','#306998','#FFE873','#FFD43B','#646464']
+    labelList = []
+    colorNumList = []
+    for catCol in cat_cols:
+        labelListTemp =  list(set(df[catCol].values))
+        colorNumList.append(len(labelListTemp))
+        labelList = labelList + labelListTemp
+        
+    # remove duplicates from labelList
+    labelList = list(dict.fromkeys(labelList))
+    
+    # define colors based on number of levels
+    colorList = []
+    for idx, colorNum in enumerate(colorNumList):
+        colorList = colorList + [colorPalette[idx]]*colorNum
+        
+    # transform df into a source-target pair
+    for i in range(len(cat_cols)-1):
+        if i==0:
+            sourceTargetDf = df[[cat_cols[i],cat_cols[i+1],value_cols]]
+            sourceTargetDf.columns = ['source','target','count']
+        else:
+            tempDf = df[[cat_cols[i],cat_cols[i+1],value_cols]]
+            tempDf.columns = ['source','target','count']
+            sourceTargetDf = pd.concat([sourceTargetDf,tempDf])
+        sourceTargetDf = sourceTargetDf.groupby(['source','target']).agg({'count':'sum'}).reset_index()
+        
+    # add index for source-target pair
+    sourceTargetDf['sourceID'] = sourceTargetDf['source'].apply(lambda x: labelList.index(x))
+    sourceTargetDf['targetID'] = sourceTargetDf['target'].apply(lambda x: labelList.index(x))
+    
+    # creating the sankey diagram
+    data = dict(
+        type='sankey',
+        node = dict(
+          pad = 15,
+          thickness = 20,
+          line = dict(
+            color = "black",
+            width = 0.5
+          ),
+          label = labelList,
+          color = colorList
+        ),
+        link = dict(
+          source = sourceTargetDf['sourceID'],
+          target = sourceTargetDf['targetID'],
+          value = sourceTargetDf['count']
+        )
+      )
+    
+    layout =  dict(
+        title = title,
+        font = dict(
+          size = 10
+        )
+    )
+       
+    fig = dict(data=[data], layout=layout)
+    return go.Figure(fig)
+
+def load_make_plots(breast_ads_orig):
+    def make_plots(patients):
+        breast_ads_plot = breast_ads_orig[breast_ads_orig['patientId'].isin( patients)]
+        fig = px.histogram(breast_ads_plot, x="diagnosis_date")
+        fig.show()
+        breast_ads_plot['count']=1
+        fig = genSankey(breast_ads_plot, cat_cols=['region','race','icdo3_topography' ],value_cols='count', title="Population Characteristics")
+        fig.show()
+    return make_plots
 
 def explode_column(df, col_name):
     df2 = df.explode(col_name)
