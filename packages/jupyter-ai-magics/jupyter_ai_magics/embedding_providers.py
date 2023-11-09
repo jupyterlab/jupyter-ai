863a8c409204f1aa929c5ef53827f1c977e51fb8
Piyush Jain <piyushjain@duck.com>
2023-11-09 13:41:32 -0800

    Backport PR #454: Add gpt4all local models, including an embedding provider

--- packages/jupyter-ai-magics/jupyter_ai_magics/embedding_providers.py
+++ packages/jupyter-ai-magics/jupyter_ai_magics/embedding_providers.py
@@ -1,3 +1,4 @@
+import os
 from typing import ClassVar, List
 
 from jupyter_ai_magics.providers import (
@@ -9,6 +10,7 @@ from jupyter_ai_magics.providers import (
 from langchain.embeddings import (
     BedrockEmbeddings,
     CohereEmbeddings,
+    GPT4AllEmbeddings,
     HuggingFaceHubEmbeddings,
     OpenAIEmbeddings,
 )
@@ -103,3 +105,25 @@ class BedrockEmbeddingsProvider(BaseEmbeddingsProvider, BedrockEmbeddings):
     model_id_key = "model_id"
     pypi_package_deps = ["boto3"]
     auth_strategy = AwsAuthStrategy()
+
+
+class GPT4AllEmbeddingsProvider(BaseEmbeddingsProvider, GPT4AllEmbeddings):
+    def __init__(self, **kwargs):
+        from gpt4all import GPT4All
+
+        model_name = kwargs.get("model_id").split(":")[-1]
+
+        # GPT4AllEmbeddings doesn't allow any kwargs at the moment
+        # This will cause the class to start downloading the model
+        # if the model file is not present. Calling retrieve_model
+        # here will throw an exception if the file is not present.
+        GPT4All.retrieve_model(model_name=model_name, allow_download=False)
+
+        kwargs["allow_download"] = False
+        super().__init__(**kwargs)
+
+    id = "gpt4all"
+    name = "GPT4All Embeddings"
+    models = ["all-MiniLM-L6-v2-f16"]
+    model_id_key = "model_id"
+    pypi_package_deps = ["gpt4all"]
