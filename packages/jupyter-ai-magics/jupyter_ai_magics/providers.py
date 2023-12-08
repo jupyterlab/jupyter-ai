1d078eb5b660662c677a7d155c6aecd5d4d58418
Jason Weill <93281816+JasonWeill@users.noreply.github.com>
2023-12-08 12:13:23 -0800

    Backport PR #499: Adds new models to Bedrock provider

--- packages/jupyter-ai-magics/jupyter_ai_magics/providers.py
+++ packages/jupyter-ai-magics/jupyter_ai_magics/providers.py
@@ -656,7 +656,10 @@ class BedrockProvider(BaseProvider, Bedrock):
         "amazon.titan-text-express-v1",
         "ai21.j2-ultra-v1",
         "ai21.j2-mid-v1",
+        "cohere.command-light-text-v14",
         "cohere.command-text-v14",
+        "meta.llama2-13b-chat-v1",
+        "meta.llama2-70b-chat-v1",
     ]
     model_id_key = "model_id"
     pypi_package_deps = ["boto3"]
@@ -680,6 +683,7 @@ class BedrockChatProvider(BaseProvider, BedrockChat):
     models = [
         "anthropic.claude-v1",
         "anthropic.claude-v2",
+        "anthropic.claude-v2:1",
         "anthropic.claude-instant-v1",
     ]
     model_id_key = "model_id"
