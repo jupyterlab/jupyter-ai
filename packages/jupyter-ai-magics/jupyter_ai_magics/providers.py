afb138e8e0519cbdd066b6ee0d90623703fcaa4d
Lumberbot (aka Jack) <39504233+meeseeksmachine@users.noreply.github.com>
2023-12-26 21:40:47 +0100

    Backport PR #540: Add gpt-4-1106-preview model from openai (#545)
    Co-authored-by: James Jun <jamesjun@fb.com>

--- packages/jupyter-ai-magics/jupyter_ai_magics/providers.py
+++ packages/jupyter-ai-magics/jupyter_ai_magics/providers.py
@@ -555,6 +555,7 @@ class ChatOpenAIProvider(BaseProvider, OpenAIChat):
         "gpt-4-32k",
         "gpt-4-32k-0314",
         "gpt-4-32k-0613",
+        "gpt-4-1106-preview",
     ]
     model_id_key = "model_name"
     pypi_package_deps = ["openai"]
@@ -596,6 +597,7 @@ class ChatOpenAINewProvider(BaseProvider, ChatOpenAI):
         "gpt-4-32k",
         "gpt-4-32k-0314",
         "gpt-4-32k-0613",
+        "gpt-4-1106-preview",
     ]
     model_id_key = "model_name"
     pypi_package_deps = ["openai"]
