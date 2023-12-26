517fbe321b500166a913a2aade82cbfdd03b1a59
David L. Qiu <david@qiu.dev>
2023-12-26 12:24:55 -0800

    WIP: bump version in root pyproject

--- scripts/bump-version.sh
+++ scripts/bump-version.sh
@@ -1,15 +1,22 @@
 #!/bin/bash
 
 # script that bumps version for all projects regardless of whether they were
-# changed since last release. needed because `lerna version` only bumps versions for projects
-# listed by `lerna changed` by default.
+# changed since last release.
+
+set -euxo pipefail
+target_version="$1"
+
+# bump version in pyproject.toml
+# TODO
+
+# we specify `--force-publish` because otherwise, `lerna version` only bumps
+# versions for projects listed by `lerna changed`.
 #
 # see: https://github.com/lerna/lerna/issues/2369
-
 (npx -p lerna@6.4.1 -y lerna version \
     --no-git-tag-version \
     --no-push \
     --force-publish \
     -y \
-    $1 \
+    "$target_version" \
 ) || exit 1
