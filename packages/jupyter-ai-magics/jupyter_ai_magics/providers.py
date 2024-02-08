0c768191dc3dcafe962654fd486f1bb44533fbe4
EduardDurech <39579228+EduardDurech@users.noreply.github.com>
2024-02-09 00:05:32 +0100

    Backport PR #625: Add new OpenAI models

--- packages/jupyter-ai-magics/jupyter_ai_magics/providers.py
+++ packages/jupyter-ai-magics/jupyter_ai_magics/providers.py
@@ -608,15 +608,18 @@ class ChatOpenAIProvider(BaseProvider, ChatOpenAI):
     name = "OpenAI"
     models = [
         "gpt-3.5-turbo",
+        "gpt-3.5-turbo-0125",
         "gpt-3.5-turbo-0301",  # Deprecated as of 2024-06-13
         "gpt-3.5-turbo-0613",  # Deprecated as of 2024-06-13
         "gpt-3.5-turbo-1106",
         "gpt-3.5-turbo-16k",
         "gpt-3.5-turbo-16k-0613",  # Deprecated as of 2024-06-13
         "gpt-4",
+        "gpt-4-turbo-preview",
         "gpt-4-0613",
         "gpt-4-32k",
         "gpt-4-32k-0613",
+        "gpt-4-0125-preview",
         "gpt-4-1106-preview",
     ]
     model_id_key = "model_name"
