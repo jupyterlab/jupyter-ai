c243591360ca034ca55609e5f9e1f6d3ad522ec6
Qiusheng Wu <giswqs@gmail.com>
2024-03-02 23:25:22 -0500

    Use VertexAI

--- packages/jupyter-ai-magics/jupyter_ai_magics/providers.py
+++ packages/jupyter-ai-magics/jupyter_ai_magics/providers.py
@@ -39,7 +39,7 @@ from langchain_community.llms import (
     SagemakerEndpoint,
     Together,
 )
-from langchain_google_genai import ChatGoogleGenerativeAI, GoogleGenerativeAI
+from langchain_google_vertexai import VertexAI
 
 # this is necessary because `langchain.pydantic_v1.main` does not include
 # `ModelMetaclass`, as it is not listed in `__all__` by the `pydantic.main`
@@ -903,14 +903,14 @@ class QianfanProvider(BaseProvider, QianfanChatEndpoint):
     auth_strategy = MultiEnvAuthStrategy(names=["QIANFAN_AK", "QIANFAN_SK"])
 
 
-class GeminiProvider(BaseProvider, GoogleGenerativeAI):
+class GeminiProvider(BaseProvider, VertexAI):
     id = "gemini"
     name = "Gemini"
     models = [
         "gemini-pro",
     ]
-    model_id_key = "model"
-    pypi_package_deps = ["langchain-google-genai"]
+    model_id_key = "model_name"
+    pypi_package_deps = ["langchain-google-vertexai"]
     auth_strategy = EnvAuthStrategy(name="GOOGLE_API_KEY")
 
     @classmethod
