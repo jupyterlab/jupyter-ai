16f39ec309c859b9b5f5008453e9168aaa5e4702
Bruce Abbott <641138+abbott@users.noreply.github.com>
2024-03-07 19:26:01 -0500

    replaced jpserver_extensions with extension_manager API

--- packages/jupyter-ai/jupyter_ai/handlers.py
+++ packages/jupyter-ai/jupyter_ai/handlers.py
@@ -116,10 +116,14 @@ class RootChatHandler(JupyterHandler, websocket.WebSocketHandler):
         """Retrieves the current user. If `jupyter_collaboration` is not
         installed, one is synthesized from the server's current shell
         environment."""
-        collaborative = self.config.ServerApp.jpserver_extensions.get(
-            "jupyter_collaboration", False
+        # Get a dictionary of all loaded extensions. 
+        # (`serverapp` is a property on all `JupyterHandler` subclasses)
+        extensions = self.serverapp.extension_manager.extensions
+        collaborative = (
+            "jupyter_collaboration" in extensions
+            and extensions["jupyter_collaboration"].enabled
         )
-
+        
         if collaborative:
             names = self.current_user.name.split(" ", maxsplit=2)
             initials = getattr(self.current_user, "initials", None)
