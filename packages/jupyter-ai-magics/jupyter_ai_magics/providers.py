d294c683661a8364f465bf9faf9a2a29aba65056
Sanjiv Das <srdas@scu.edu>
2024-03-20 10:24:21 -0700

    Add Claude-3-Haiku (#696)
    * Add Claude-3-Haiku
    
    Added the Claude 3 Haiku model to Anthropic Chat providers and also to Bedrock Claude Chat providers
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- packages/jupyter-ai-magics/jupyter_ai_magics/providers.py
+++ packages/jupyter-ai-magics/jupyter_ai_magics/providers.py
@@ -437,7 +437,9 @@ class AnthropicProvider(BaseProvider, Anthropic):
         return False
 
 
-class ChatAnthropicProvider(BaseProvider, ChatAnthropic):
+class ChatAnthropicProvider(
+    BaseProvider, ChatAnthropic
+):  # https://docs.anthropic.com/claude/docs/models-overview
     id = "anthropic-chat"
     name = "ChatAnthropic"
     models = [
@@ -446,6 +448,7 @@ class ChatAnthropicProvider(BaseProvider, ChatAnthropic):
         "claude-instant-1.2",
         "claude-3-opus-20240229",
         "claude-3-sonnet-20240229",
+        "claude-3-haiku-20240307",
     ]
     model_id_key = "model"
     pypi_package_deps = ["anthropic"]
@@ -829,6 +832,7 @@ class BedrockChatProvider(BaseProvider, BedrockChat):
         "anthropic.claude-v2:1",
         "anthropic.claude-instant-v1",
         "anthropic.claude-3-sonnet-20240229-v1:0",
+        "anthropic.claude-3-haiku-20240307-v1:0",
     ]
     model_id_key = "model_id"
     pypi_package_deps = ["boto3"]
