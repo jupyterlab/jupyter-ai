da5d2e5542addb059d62ed61b559a8c3c9737082
Sanjiv Das <srdas@scu.edu>
2024-05-08 13:49:03 -0700

    Backport PR #778: Add Titan embedding model v2

--- packages/jupyter-ai-magics/jupyter_ai_magics/embedding_providers.py
+++ packages/jupyter-ai-magics/jupyter_ai_magics/embedding_providers.py
@@ -98,10 +98,11 @@ class HfHubEmbeddingsProvider(BaseEmbeddingsProvider, HuggingFaceHubEmbeddings):
     registry = True
 
 
+# See model ID list here: https://docs.aws.amazon.com/bedrock/latest/userguide/model-ids.html
 class BedrockEmbeddingsProvider(BaseEmbeddingsProvider, BedrockEmbeddings):
     id = "bedrock"
     name = "Bedrock"
-    models = ["amazon.titan-embed-text-v1"]
+    models = ["amazon.titan-embed-text-v1", "amazon.titan-embed-text-v2:0"]
     model_id_key = "model_id"
     pypi_package_deps = ["boto3"]
     auth_strategy = AwsAuthStrategy()
