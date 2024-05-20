7a8addb072c8c8c7da970b18b1d6bfe10446f0fa
Sanjiv Das <srdas@scu.edu>
2024-05-20 14:32:52 -0700

    Added gpt-4o
    Updated for use with the new open ai model

--- packages/jupyter-ai-magics/jupyter_ai_magics/partner_providers/openai.py
+++ packages/jupyter-ai-magics/jupyter_ai_magics/partner_providers/openai.py
@@ -24,7 +24,7 @@ class OpenAIProvider(BaseProvider, OpenAI):
             return error_details.get("code") == "invalid_api_key"
         return False
 
-
+# https://platform.openai.com/docs/models/
 class ChatOpenAIProvider(BaseProvider, ChatOpenAI):
     id = "openai-chat"
     name = "OpenAI"
@@ -37,12 +37,14 @@ class ChatOpenAIProvider(BaseProvider, ChatOpenAI):
         "gpt-3.5-turbo-16k",
         "gpt-3.5-turbo-16k-0613",  # Deprecated as of 2024-06-13
         "gpt-4",
+        "gpt-4-turbo",
         "gpt-4-turbo-preview",
         "gpt-4-0613",
         "gpt-4-32k",
         "gpt-4-32k-0613",
         "gpt-4-0125-preview",
         "gpt-4-1106-preview",
+        "gpt-4o",
     ]
     model_id_key = "model_name"
     pypi_package_deps = ["langchain_openai"]
