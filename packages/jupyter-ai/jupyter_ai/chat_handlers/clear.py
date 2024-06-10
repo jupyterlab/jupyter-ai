83ca89ee02fdcb8e92131bd841f0980a67ce7ed3
Sanjiv Das <srdas@scu.edu>
2024-06-10 14:06:09 -0700

    Update chat handling
    (1) Make sure to use the `chat_history` prefix in the filename if none is supplied and not use a blank string on double `<Enter>`
    
    (2) When using `/clear` clear the entire chat but show the help menu.

--- packages/jupyter-ai/jupyter_ai/chat_handlers/clear.py
+++ packages/jupyter-ai/jupyter_ai/chat_handlers/clear.py
@@ -4,8 +4,8 @@ from jupyter_ai.models import ChatMessage, ClearMessage
 
 from .base import BaseChatHandler, SlashCommandRoutingType
 
-
 class ClearChatHandler(BaseChatHandler):
+    """Clear the chat panel and show the help menu"""
     id = "clear"
     name = "Clear chat messages"
     help = "Clear the chat window"
@@ -17,10 +17,15 @@ class ClearChatHandler(BaseChatHandler):
         super().__init__(*args, **kwargs)
 
     async def process_message(self, _):
+        tmp_chat_history = self._chat_history[0]
         self._chat_history.clear()
         for handler in self._root_chat_handlers.values():
             if not handler:
                 continue
 
             handler.broadcast_message(ClearMessage())
+
+            self._chat_history = [tmp_chat_history]
+            self.reply(tmp_chat_history.body)
+            
             break
