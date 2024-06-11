e214ee70ad9b33a999b64b9740ba739b61f6d243
Jeremy Tuloup <jeremy.tuloup@gmail.com>
2024-06-11 21:37:22 +0200

    Add support for MistralAI (#823)
    * Add support for MistralAI
    
    * embeddings
    
    * lint

---
+++ packages/jupyter-ai-magics/jupyter_ai_magics/partner_providers/mistralai.py
@@ -0,0 +1,32 @@
+from jupyter_ai_magics.providers import BaseProvider, EnvAuthStrategy
+from langchain_mistralai import ChatMistralAI, MistralAIEmbeddings
+
+from ..embedding_providers import BaseEmbeddingsProvider
+
+
+class MistralAIProvider(BaseProvider, ChatMistralAI):
+    id = "mistralai"
+    name = "MistralAI"
+    models = [
+        "open-mistral-7b",
+        "open-mixtral-8x7b",
+        "open-mixtral-8x22b",
+        "mistral-small-latest",
+        "mistral-medium-latest",
+        "mistral-large-latest",
+        "codestral-latest",
+    ]
+    model_id_key = "model"
+    auth_strategy = EnvAuthStrategy(name="MISTRAL_API_KEY")
+    pypi_package_deps = ["langchain-mistralai"]
+
+
+class MistralAIEmbeddingsProvider(BaseEmbeddingsProvider, MistralAIEmbeddings):
+    id = "mistralai"
+    name = "MistralAI"
+    models = [
+        "mistral-embed",
+    ]
+    model_id_key = "model"
+    pypi_package_deps = ["langchain-mistralai"]
+    auth_strategy = EnvAuthStrategy(name="MISTRAL_API_KEY")
