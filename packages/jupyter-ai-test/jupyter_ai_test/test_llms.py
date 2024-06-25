c8d592bddc9b33882d9fa377f9bc0c0794759695
David L. Qiu <david@qiu.dev>
2024-06-25 15:54:16 -0700

    add jupyter_ai_test package for developer testing

---
+++ packages/jupyter-ai-test/jupyter_ai_test/test_llms.py
@@ -0,0 +1,55 @@
+import time
+from typing import Any, List, Optional, Iterator
+
+from langchain_core.callbacks.manager import CallbackManagerForLLMRun
+from langchain_core.language_models.llms import LLM
+from langchain_core.outputs.generation import GenerationChunk
+
+class TestLLM(LLM):
+    model_id: str = "test"
+
+    @property
+    def _llm_type(self) -> str:
+        return "custom"
+
+    def _call(
+        self,
+        prompt: str,
+        stop: Optional[List[str]] = None,
+        run_manager: Optional[CallbackManagerForLLMRun] = None,
+        **kwargs: Any,
+    ) -> str:
+        time.sleep(3)
+        return f"Hello! This is a dummy response from a test LLM."
+    
+class TestLLMWithStreaming(LLM):
+    model_id: str = "test"
+
+    @property
+    def _llm_type(self) -> str:
+        return "custom"
+
+    def _call(
+        self,
+        prompt: str,
+        stop: Optional[List[str]] = None,
+        run_manager: Optional[CallbackManagerForLLMRun] = None,
+        **kwargs: Any,
+    ) -> str:
+        time.sleep(3)
+        return f"Hello! This is a dummy response from a test LLM."
+
+    def _stream(
+        self,
+        prompt: str,
+        stop: Optional[List[str]] = None,
+        run_manager: Optional[CallbackManagerForLLMRun] = None,
+        **kwargs: Any,
+    ) -> Iterator[GenerationChunk]:
+        time.sleep(5)
+        yield GenerationChunk(text="Hello! This is a dummy response from a test LLM. I will now count from 1 to 100.\n\n")
+        for i in range(1, 101):
+            time.sleep(0.5)
+            yield GenerationChunk(text=f"{i}, ")
+    
+    
