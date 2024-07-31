b9535688a324fc13143b15b4ba1aaa539977861f
david qiu <david@qiu.dev>
2024-07-31 18:34:57 -0400

    Add support for custom/provisioned models in Bedrock (#922)
    * add BedrockCustomProvider
    
    * pre-commit
    
    * format help text
    
    * show model ID field
    
    * edit BedrockCustomProvider help text
    
    * add mention of BedrockCustom in docs

--- packages/jupyter-ai-magics/jupyter_ai_magics/partner_providers/aws.py
+++ packages/jupyter-ai-magics/jupyter_ai_magics/partner_providers/aws.py
@@ -104,6 +104,30 @@ class BedrockChatProvider(BaseProvider, ChatBedrock):
         return not "anthropic" in self.model_id
 
 
+class BedrockCustomProvider(BaseProvider, ChatBedrock):
+    id = "bedrock-custom"
+    name = "Amazon Bedrock (custom/provisioned)"
+    models = ["*"]
+    model_id_key = "model_id"
+    model_id_label = "Model ID"
+    pypi_package_deps = ["langchain-aws"]
+    auth_strategy = AwsAuthStrategy()
+    fields = [
+        TextField(key="provider", label="Provider (required)", format="text"),
+        TextField(key="region_name", label="Region name (optional)", format="text"),
+        TextField(
+            key="credentials_profile_name",
+            label="AWS profile (optional)",
+            format="text",
+        ),
+    ]
+    help = (
+        "Specify the ARN (Amazon Resource Name) of the custom/provisioned model as the model ID. For more information, see the [Amazon Bedrock model IDs documentation](https://docs.aws.amazon.com/bedrock/latest/userguide/model-ids.html).\n\n"
+        "The model provider must also be specified below. This is the provider of your foundation model *in lowercase*, e.g. `amazon`, `anthropic`, `meta`, or `mistral`."
+    )
+    registry = True
+
+
 # See model ID list here: https://docs.aws.amazon.com/bedrock/latest/userguide/model-ids.html
 class BedrockEmbeddingsProvider(BaseEmbeddingsProvider, BedrockEmbeddings):
     id = "bedrock"
