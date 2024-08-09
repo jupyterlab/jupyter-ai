40819d21da6b0bd1b9c9b68744af2fca62999f70
michael <michael_chia7@msn.com>
2024-08-09 17:33:18 +0800

    fix chat history class

--- packages/jupyter-ai/jupyter_ai/history.py
+++ packages/jupyter-ai/jupyter_ai/history.py
@@ -1,4 +1,4 @@
-from typing import List, Sequence
+from typing import List, Sequence, Optional
 
 from langchain_core.chat_history import BaseChatMessageHistory
 from langchain_core.messages import BaseMessage
@@ -14,27 +14,27 @@ class BoundedChatHistory(BaseChatMessageHistory, BaseModel):
     messages and 2 AI messages.
     """
 
-    _messages: List[BaseMessage] = Field(default_factory=list)
+    all_messages: List[BaseMessage] = Field(default_factory=list, alias='messages')
     size: int = 0
     k: int
 
     @property
     def messages(self) -> List[BaseMessage]:
-        return self._messages[-self.k * 2 :]
+        return self.all_messages[-self.k * 2 :]
 
     async def aget_messages(self) -> List[BaseMessage]:
         return self.messages
 
     def add_message(self, message: BaseMessage) -> None:
         """Add a self-created message to the store"""
-        self._messages.append(message)
+        self.all_messages.append(message)
 
     async def aadd_messages(self, messages: Sequence[BaseMessage]) -> None:
         """Add messages to the store"""
         self.add_messages(messages)
 
     def clear(self) -> None:
-        self._messages = []
+        self.all_messages = []
 
     async def aclear(self) -> None:
         self.clear()
