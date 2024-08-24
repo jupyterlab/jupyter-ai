2d24a60316b1fdee497388fce2e4593428d7c23d
trducng <trungduc1992@gmail.com>
2024-08-24 21:45:05 +0700

    Add Gemini 1.5 to the list of chat options

--- packages/jupyter-ai-magics/jupyter_ai_magics/partner_providers/gemini.py
+++ packages/jupyter-ai-magics/jupyter_ai_magics/partner_providers/gemini.py
@@ -6,6 +6,8 @@ class GeminiProvider(BaseProvider, GoogleGenerativeAI):
     id = "gemini"
     name = "Gemini"
     models = [
+        "gemini-1.5-pro",
+        "gemini-1.5-flash",
         "gemini-1.0-pro",
         "gemini-1.0-pro-001",
         "gemini-1.0-pro-latest",
