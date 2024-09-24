4043f36f7d3cf56a02dd4152001ed25c86280545
Sanjiv Das <srdas@scu.edu>
2024-09-24 12:01:32 -0700

    Update tools.py

--- packages/jupyter-ai/jupyter_ai/chat_handlers/tools.py
+++ packages/jupyter-ai/jupyter_ai/chat_handlers/tools.py
@@ -226,9 +226,9 @@ class ToolsChatHandler(BaseChatHandler):
         try:
             self.model_with_tools = self.llm.__class__(
                 model_id=self.llm.model_id
-            ).bind_tools(
+            ).bind_tools( # type:ignore[attr-defined]
                 tools
-            )  # type:ignore[attr-defined]
+            )  
         except AttributeError:
             raise ExceptionModelDoesTakeTools()
         except Exception:
