bea28989f4166241489a6019c91588b68e4dd9a8
Sanjiv Das <srdas@scu.edu>
2024-09-24 10:19:07 -0700

    Update tools.py

--- packages/jupyter-ai/jupyter_ai/chat_handlers/tools.py
+++ packages/jupyter-ai/jupyter_ai/chat_handlers/tools.py
@@ -170,7 +170,7 @@ class ToolsChatHandler(BaseChatHandler):
             """
             messages = state["messages"]
             last_message = messages[-1]
-            if last_message.tool_calls:
+            if len(last_message.tool_calls)>0:
                 return "tools"
             return "__end__"
 
