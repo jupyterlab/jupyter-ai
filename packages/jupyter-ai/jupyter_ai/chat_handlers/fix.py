117a7bee68d9bee6f76daf3a97250744b3230d6a
Lumberbot (aka Jack) <39504233+meeseeksmachine@users.noreply.github.com>
2024-10-28 16:41:02 -0700

    Backport PR #1048: Implement streaming for `/fix` (#1059)
    Co-authored-by: Sanjiv Das <srdas@scu.edu>

--- packages/jupyter-ai/jupyter_ai/chat_handlers/fix.py
+++ packages/jupyter-ai/jupyter_ai/chat_handlers/fix.py
@@ -2,7 +2,6 @@ from typing import Dict, Type
 
 from jupyter_ai.models import CellWithErrorSelection, HumanChatMessage
 from jupyter_ai_magics.providers import BaseProvider
-from langchain.chains import LLMChain
 from langchain.prompts import PromptTemplate
 
 from .base import BaseChatHandler, SlashCommandRoutingType
@@ -64,6 +63,7 @@ class FixChatHandler(BaseChatHandler):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        self.prompt_template = None
 
     def create_llm_chain(
         self, provider: Type[BaseProvider], provider_params: Dict[str, str]
@@ -73,13 +73,11 @@ class FixChatHandler(BaseChatHandler):
             **(self.get_model_parameters(provider, provider_params)),
         }
         llm = provider(**unified_parameters)
-
         self.llm = llm
-        # TODO: migrate this class to use a LCEL `Runnable` instead of
-        # `Chain`, then remove the below ignore comment.
-        self.llm_chain = LLMChain(  # type:ignore[arg-type]
-            llm=llm, prompt=FIX_PROMPT_TEMPLATE, verbose=True
-        )
+        prompt_template = FIX_PROMPT_TEMPLATE
+
+        runnable = prompt_template | llm  # type:ignore
+        self.llm_chain = runnable
 
     async def process_message(self, message: HumanChatMessage):
         if not (message.selection and message.selection.type == "cell-with-error"):
@@ -96,16 +94,13 @@ class FixChatHandler(BaseChatHandler):
         extra_instructions = message.prompt[4:].strip() or "None."
 
         self.get_llm_chain()
-        with self.pending("Analyzing error", message):
-            assert self.llm_chain
-            # TODO: migrate this class to use a LCEL `Runnable` instead of
-            # `Chain`, then remove the below ignore comment.
-            response = await self.llm_chain.apredict(  # type:ignore[attr-defined]
-                extra_instructions=extra_instructions,
-                stop=["\nHuman:"],
-                cell_content=selection.source,
-                error_name=selection.error.name,
-                error_value=selection.error.value,
-                traceback="\n".join(selection.error.traceback),
-            )
-        self.reply(response, message)
+        assert self.llm_chain
+
+        inputs = {
+            "extra_instructions": extra_instructions,
+            "cell_content": selection.source,
+            "traceback": "\n".join(selection.error.traceback),
+            "error_name": selection.error.name,
+            "error_value": selection.error.value,
+        }
+        await self.stream_reply(inputs, message, pending_msg="Analyzing error")
