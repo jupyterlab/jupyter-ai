d3fbb51728979d1db416b970af2eaf85bbcb6a6c
Sanjiv Das <srdas@scu.edu>
2024-11-06 09:42:14 -0800

    Anthropic models added

--- packages/jupyter-ai-magics/jupyter_ai_magics/partner_providers/anthropic.py
+++ packages/jupyter-ai-magics/jupyter_ai_magics/partner_providers/anthropic.py
@@ -15,6 +15,7 @@ class ChatAnthropicProvider(
         "claude-3-sonnet-20240229",
         "claude-3-haiku-20240307",
         "claude-3-5-haiku-20241022",
+        "claude-3-5-sonnet-20240620",
         "claude-3-5-sonnet-20241022",
     ]
     model_id_key = "model"
