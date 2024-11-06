7e17e6b21dfd22c340ff5ed6943740a0f73c4658
Sanjiv Das <srdas@scu.edu>
2024-11-06 09:59:43 -0800

    Remove retired models and add new `Haiku-3.5` model in Anthropic (#1092)
    * Updated `anthropic.py` to remove deprecated models, remove `AnthropicProvider` class, add new models
    
    Removed all deprecated models as listed here: https://docs.anthropic.com/en/docs/resources/model-deprecations.
    
    This makes the `AnthropicProvider` class redundant, so it is now removed. The corresponding entry in `pyproject.toml` is also deleted.
    
    The class `ChatAnthropicProvider` is updated to include the new `Claude-3.5-Haiku`model, and the function to check for  API key exception has been added.  The older version of `Claude-3.5-sonnet` has also been removed.
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Anthropic models added
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- packages/jupyter-ai-magics/jupyter_ai_magics/partner_providers/aws.py
+++ packages/jupyter-ai-magics/jupyter_ai_magics/partner_providers/aws.py
@@ -68,6 +68,7 @@ class BedrockChatProvider(BaseProvider, ChatBedrock):
         "anthropic.claude-3-sonnet-20240229-v1:0",
         "anthropic.claude-3-haiku-20240307-v1:0",
         "anthropic.claude-3-opus-20240229-v1:0",
+        "anthropic.claude-3-5-haiku-20241022-v1:0",
         "anthropic.claude-3-5-sonnet-20240620-v1:0",
         "anthropic.claude-3-5-sonnet-20241022-v2:0",
         "meta.llama2-13b-chat-v1",
