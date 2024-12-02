jupyterlab/jupyter-ai

commit 84c074a15a333a42623d75055792ca45a1ee105b

Setting default model providers (#421)
* Setting default model providers

* Setting default model providers

* [pre-commit.ci] auto fixes from pre-commit.com hooks

for more information, see https://pre-commit.ci

* Setting default model providers

* [pre-commit.ci] auto fixes from pre-commit.com hooks

for more information, see https://pre-commit.ci

* Added default_ prefix to new attributes

* Added default_ prefix to docs as well

* Fix docs

* Removed redundant code

* Incorporated naming related comments

* [pre-commit.ci] auto fixes from pre-commit.com hooks

for more information, see https://pre-commit.ci

* Removing unnecessary files

* Corrected config names, simplified docs.

---------

Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
Co-authored-by: Piyush Jain <piyushjain@duck.com>

packages/jupyter-ai/jupyter_ai/config_manager.py
@@ -105,6 +105,7 @@ class ConfigManager(Configurable):
         blocked_providers: Optional[List[str]],
         allowed_models: Optional[List[str]],
         blocked_models: Optional[List[str]],
+        defaults: dict,
         *args,
         **kwargs,
     ):
@@ -120,6 +121,8 @@ class ConfigManager(Configurable):
         self._blocked_providers = blocked_providers
         self._allowed_models = allowed_models
         self._blocked_models = blocked_models
+        self._defaults = defaults
+        """Provider defaults."""
 
         self._last_read: Optional[int] = None
         """When the server last read the config file. If the file was not
@@ -146,14 +149,20 @@ class ConfigManager(Configurable):
             self.validator = Validator(schema)
 
     def _init_config(self):
+        default_config = self._init_defaults()
         if os.path.exists(self.config_path):
-            self._process_existing_config()
+            self._process_existing_config(default_config)
         else:
-            self._create_default_config()
+            self._create_default_config(default_config)
 
-    def _process_existing_config(self):
+    def _process_existing_config(self, default_config):
         with open(self.config_path, encoding="utf-8") as f:
-            config = GlobalConfig(**json.loads(f.read()))
+            existing_config = json.loads(f.read())
+            merged_config = Merger.merge(
+                default_config,
+                {k: v for k, v in existing_config.items() if v is not None},
+            )
+            config = GlobalConfig(**merged_config)
             validated_config = self._validate_lm_em_id(config)
 
             # re-write to the file to validate the config and apply any
@@ -192,14 +201,23 @@ class ConfigManager(Configurable):
 
         return config
 
-    def _create_default_config(self):
-        properties = self.validator.schema.get("properties", {})
+    def _create_default_config(self, default_config):
+        self._write_config(GlobalConfig(**default_config))
+
+    def _init_defaults(self):
         field_list = GlobalConfig.__fields__.keys()
+        properties = self.validator.schema.get("properties", {})
         field_dict = {
             field: properties.get(field).get("default") for field in field_list
         }
-        default_config = GlobalConfig(**field_dict)
-        self._write_config(default_config)
+        if self._defaults is None:
+            return field_dict
+
+        for field in field_list:
+            default_value = self._defaults.get(field)
+            if default_value is not None:
+                field_dict[field] = default_value
+        return field_dict
 
     def _read_config(self) -> GlobalConfig:
         """Returns the user's current configuration as a GlobalConfig object.

packages/jupyter-ai/jupyter_ai/tests/test_config_manager.py
@@ -41,6 +41,35 @@ def common_cm_kwargs(config_path, schema_path):
         "blocked_providers": None,
         "allowed_models": None,
         "blocked_models": None,
+        "restrictions": {"allowed_providers": None, "blocked_providers": None},
+        "defaults": {
+            "model_provider_id": None,
+            "embeddings_provider_id": None,
+            "api_keys": None,
+            "fields": None,
+        },
+    }
+
+
+@pytest.fixture
+def cm_kargs_with_defaults(config_path, schema_path, common_cm_kwargs):
+    """Kwargs that are commonly used when initializing the CM."""
+    log = logging.getLogger()
+    lm_providers = get_lm_providers()
+    em_providers = get_em_providers()
+    return {
+        **common_cm_kwargs,
+        "defaults": {
+            "model_provider_id": "bedrock-chat:anthropic.claude-v1",
+            "embeddings_provider_id": "bedrock:amazon.titan-embed-text-v1",
+            "api_keys": {"OPENAI_API_KEY": "open-ai-key-value"},
+            "fields": {
+                "bedrock-chat:anthropic.claude-v1": {
+                    "credentials_profile_name": "default",
+                    "region_name": "us-west-2",
+                }
+            },
+        },
     }
 
 
@@ -68,6 +97,12 @@ def cm_with_allowlists(common_cm_kwargs):
         "allowed_models": ["cohere:medium"],
     }
     return ConfigManager(**kwargs)
+
+
+@pytest.fixture
+def cm_with_defaults(cm_kargs_with_defaults):
+    """The default ConfigManager instance, with an empty config and config schema."""
+    return ConfigManager(**cm_kargs_with_defaults)
 
 
 @pytest.fixture(autouse=True)
@@ -182,6 +217,51 @@ def test_init_with_allowlists(cm: ConfigManager, common_cm_kwargs):
     assert test_cm._allowed_models == None
     assert test_cm.lm_gid == None
     assert test_cm.em_gid == None
+
+
+def test_init_with_default_values(
+    cm_with_defaults: ConfigManager,
+    config_path: str,
+    schema_path: str,
+    common_cm_kwargs,
+):
+    """
+    Test that the ConfigManager initializes with the expected default values.
+
+    Args:
+        cm_with_defaults (ConfigManager): A ConfigManager instance with default values.
+        config_path (str): The path to the configuration file.
+        schema_path (str): The path to the schema file.
+    """
+    config_response = cm_with_defaults.get_config()
+    # assert config response
+    assert config_response.model_provider_id == "bedrock-chat:anthropic.claude-v1"
+    assert (
+        config_response.embeddings_provider_id == "bedrock:amazon.titan-embed-text-v1"
+    )
+    assert config_response.api_keys == ["OPENAI_API_KEY"]
+    assert config_response.fields == {
+        "bedrock-chat:anthropic.claude-v1": {
+            "credentials_profile_name": "default",
+            "region_name": "us-west-2",
+        }
+    }
+
+    del cm_with_defaults
+
+    log = logging.getLogger()
+    lm_providers = get_lm_providers()
+    em_providers = get_em_providers()
+    kwargs = {
+        **common_cm_kwargs,
+        "defaults": {"model_provider_id": "bedrock-chat:anthropic.claude-v2"},
+    }
+    cm_with_defaults_override = ConfigManager(**kwargs)
+
+    assert (
+        cm_with_defaults_override.get_config().model_provider_id
+        == "bedrock-chat:anthropic.claude-v1"
+    )
 
 
 def test_property_access_on_default_config(cm: ConfigManager):

packages/jupyter-ai/jupyter_ai/extension.py
@@ -106,6 +106,38 @@ class AiExtension(ExtensionApp):
         config=True,
     )
 
+    default_language_model = Unicode(
+        default_value=None,
+        allow_none=True,
+        help="""
+        Default language model to use, as string in the format
+        <provider-id>:<model-id>, defaults to None.
+        """,
+        config=True,
+    )
+
+    default_embeddings_model = Unicode(
+        default_value=None,
+        allow_none=True,
+        help="""
+        Default embeddings model to use, as string in the format
+        <provider-id>:<model-id>, defaults to None.
+        """,
+        config=True,
+    )
+
+    default_api_keys = Dict(
+        key_trait=Unicode(),
+        value_trait=Unicode(),
+        default_value=None,
+        allow_none=True,
+        help="""
+        Default API keys for model providers, as a dictionary,
+        in the format `<key-name>:<key-value>`. Defaults to None.
+        """,
+        config=True,
+    )
+
     def initialize_settings(self):
         start = time.time()
 
@@ -123,6 +155,13 @@ class AiExtension(ExtensionApp):
 
         self.settings["model_parameters"] = self.model_parameters
         self.log.info(f"Configured model parameters: {self.model_parameters}")
+
+        defaults = {
+            "model_provider_id": self.default_language_model,
+            "embeddings_provider_id": self.default_embeddings_model,
+            "api_keys": self.default_api_keys,
+            "fields": self.model_parameters,
+        }
 
         # Fetch LM & EM providers
         self.settings["lm_providers"] = get_lm_providers(
@@ -142,6 +181,7 @@ class AiExtension(ExtensionApp):
             blocked_providers=self.blocked_providers,
             allowed_models=self.allowed_models,
             blocked_models=self.blocked_models,
+            defaults=defaults,
         )
 
         self.log.info("Registered providers.")

commit f82785e139b75c880ec0c5bc1d6721f3b451a71e

add test capturing bug introduced by #421

packages/jupyter-ai/jupyter_ai/config_manager.py
@@ -107,11 +107,11 @@ class ConfigManager(Configurable):
         log: Logger,
         lm_providers: LmProvidersDict,
         em_providers: EmProvidersDict,
-        allowed_providers: Optional[List[str]],
-        blocked_providers: Optional[List[str]],
-        allowed_models: Optional[List[str]],
-        blocked_models: Optional[List[str]],
         defaults: dict,
+        allowed_providers: Optional[List[str]] = None,
+        blocked_providers: Optional[List[str]] = None,
+        allowed_models: Optional[List[str]] = None,
+        blocked_models: Optional[List[str]] = None,
         *args,
         **kwargs,
     ):

packages/jupyter-ai/jupyter_ai/tests/test_config_manager.py
@@ -24,6 +24,29 @@ def config_path(jp_data_dir):
 def schema_path(jp_data_dir):
     return str(jp_data_dir / "config_schema.json")
 
+@pytest.fixture
+def config_file_with_model_fields(jp_data_dir):
+    """
+    Fixture that creates a `config.json` file with the chat model set to
+    `openai-chat:gpt-4o` and fields for that model. Returns path to the file.
+    """
+    config_data = {
+        "model_provider_id:": "openai-chat:gpt-4o",
+        "embeddings_provider_id": None,
+        "api_keys": {
+            "openai_api_key": "foobar"
+        },
+        "send_with_shift_enter": False,
+        "fields": {
+            "openai-chat:gpt-4o": {
+                "openai_api_base": "https://example.com"
+            }
+        },
+    }
+    config_path = jp_data_dir / "config.json"
+    with open(config_path, "w") as file:
+        json.dump(config_data, file)
+    return str(config_path)
 
 @pytest.fixture
 def common_cm_kwargs(config_path, schema_path):
@@ -426,5 +449,39 @@ def test_handle_bad_provider_ids(cm_with_bad_provider_ids):
     assert config_desc.embeddings_provider_id is None
 
 def test_config_manager_returns_fields(cm):
+    """
+    Asserts that `ConfigManager.lm_provider_params` returns model fields set by
+    the user.
+    """
     expected_model_args = configure_with_fields(cm)
     assert cm.lm_provider_params == expected_model_args
+
+def test_config_manager_does_not_write_to_defaults(config_file_with_model_fields, schema_path):
+    """
+    Asserts that `ConfigManager` does not write to the `defaults` argument when
+    the configured chat model differs from the one specified in `defaults`.
+    """
+    from copy import deepcopy
+    config_path = config_file_with_model_fields
+    log = logging.getLogger()
+    lm_providers = get_lm_providers()
+    em_providers = get_em_providers()
+
+    defaults = {
+        "model_provider_id": None,
+        "embeddings_provider_id": None,
+        "api_keys": {},
+        "fields": {}
+    }
+    expected_defaults = deepcopy(defaults)
+
+    cm = ConfigManager(
+        log=log,
+        lm_providers=lm_providers,
+        em_providers=em_providers,
+        config_path=config_path,
+        schema_path=schema_path,
+        defaults=defaults,
+    )
+
+    assert defaults == expected_defaults