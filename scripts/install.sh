2c196d957a9eaaae6b93deb558cdc63a0c669678
david qiu <david@qiu.dev>
2024-12-05 06:59:42 -0800

    Fix install step in CI (#1139)
    * fix install in CI
    
    * pre-commit

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 set -eux
-# install core packages
-pip install jupyterlab~=4.0
+
+# Install JupyterLab
+#
+# Excludes v4.3.2 as it pins `httpx` to a very narrow range, causing `pip
+# install` to stall on package resolution.
+#
+# See: https://github.com/jupyterlab/jupyter-ai/issues/1138
+pip install jupyterlab~=4.0,!=4.3.2
+
+# Install core packages
 cp playground/config.example.py playground/config.py
 jlpm install
 jlpm dev-install
