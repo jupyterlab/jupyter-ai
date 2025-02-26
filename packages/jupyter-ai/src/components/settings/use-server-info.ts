3380493bc34b19bdbb82f537a14bb9a771ef9c7b
Sanjiv Das <srdas@scu.edu>
2025-02-26 12:53:29 -0800

    Update use-server-info.ts

--- packages/jupyter-ai/src/components/settings/use-server-info.ts
+++ packages/jupyter-ai/src/components/settings/use-server-info.ts
@@ -91,7 +91,7 @@ export function useServerInfo(): ServerInfo {
         completions: {
           lmProvider: cLmProvider,
           lmLocalId: cLmLocalId,
-          emLocalId: emLocalId
+          emLocalId: emLocalId,
         }
       });
 
