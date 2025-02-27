6cea51464d73c6859bfa912b47fa96b07630bcf5
Darshan808 <pranishpoudel10@gmail.com>
2025-02-27 17:02:24 +0545

    fix-ydoc-unwanted-import

--- packages/jupyter-ai/jupyter_ai/__init__.py
+++ packages/jupyter-ai/jupyter_ai/__init__.py
@@ -1,8 +1,3 @@
-# The following import is to make sure jupyter_ydoc is imported before
-# jupyterlab_chat, otherwise it leads to circular import because of the
-# YChat relying on YBaseDoc, and jupyter_ydoc registering YChat from the entry point.
-import jupyter_ydoc
-
 # expose jupyter_ai_magics ipython extension
 # DO NOT REMOVE.
 from jupyter_ai_magics import load_ipython_extension, unload_ipython_extension
