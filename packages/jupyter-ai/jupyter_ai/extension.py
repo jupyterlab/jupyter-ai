a2562d99905e3fa7c05cdc2ff13848332065373d
Sanjiv Das <srdas@scu.edu>
2025-04-02 11:07:07 -0700

    Update extension.py

--- packages/jupyter-ai/jupyter_ai/extension.py
+++ packages/jupyter-ai/jupyter_ai/extension.py
@@ -350,22 +350,13 @@ class AiExtension(ExtensionApp):
         self.settings["model_parameters"] = self.model_parameters
         self.log.info(f"Configured model parameters: {self.model_parameters}")
 
-        # Collect embeddings model parameters and fields
-        if self.default_embeddings_model:
-            self.em_parameters = {
-                self.default_embeddings_model: self.model_parameters[
-                    self.default_embeddings_model
-                ]
-            }
-        else:
-            self.em_parameters = {}
-
         defaults = {
             "model_provider_id": self.default_language_model,
             "embeddings_provider_id": self.default_embeddings_model,
             "api_keys": self.default_api_keys,
             "fields": self.model_parameters,
-            "embeddings_fields": self.em_parameters,
+            "embeddings_fields": self.model_parameters,
+            "completions_fields": self.model_parameters,
         }
 
         # Fetch LM & EM providers
