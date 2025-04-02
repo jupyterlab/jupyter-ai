bb69b89fedd81aa25ac16949ce13e510975248bf
Lumberbot (aka Jack) <39504233+meeseeksmachine@users.noreply.github.com>
2025-04-02 16:18:33 -0700

    Backport PR #1298: Updating embeddings config fields from prespecified custom config file (#1302)
    Co-authored-by: Sanjiv Das <srdas@scu.edu>

--- packages/jupyter-ai/jupyter_ai/extension.py
+++ packages/jupyter-ai/jupyter_ai/extension.py
@@ -208,7 +208,6 @@ class AiExtension(ExtensionApp):
         self.log.info(f"Configured provider blocklist: {self.blocked_providers}")
         self.log.info(f"Configured model allowlist: {self.allowed_models}")
         self.log.info(f"Configured model blocklist: {self.blocked_models}")
-
         self.settings["model_parameters"] = self.model_parameters
         self.log.info(f"Configured model parameters: {self.model_parameters}")
 
@@ -217,6 +216,8 @@ class AiExtension(ExtensionApp):
             "embeddings_provider_id": self.default_embeddings_model,
             "api_keys": self.default_api_keys,
             "fields": self.model_parameters,
+            "embeddings_fields": self.model_parameters,
+            "completions_fields": self.model_parameters,
         }
 
         # Fetch LM & EM providers
