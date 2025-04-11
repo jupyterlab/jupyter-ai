90df7ea23450b098f3bc4d5aaa73a72025c763ec
David L. Qiu <david@qiu.dev>
2025-04-11 13:59:18 -0700

    Introduce AI personas (see PR #1324)
    add dev-reinstall script for convenience
    
    add PersonaManager and JupyternautPersona
    
    add PersonaAwareness to allow awareness on >1 persona
    
    add DebugPersona to jupyter_ai_test
    
    upgrade to Jupyter Chat v0.10.0
    
    automatically generate IDs for personas
    
    add mention-based routing to PersonaManager
    
    implement streaming replies in Jupyternaut persona
    
    correctly identify AI messages in YChatHistory
    
    fix mention routing to actually work
    
    add logging capability to PersonaAwareness
    
    update BasePersona.id to not depend on module path
    
    improve logging in PersonaManager
    
    log time elapsed in PersonaManager
    
    simplify jupyternaut prompt template
    
    pre-commit
    
    add comment to ref jupyter-chat#212
    
    have _init_persona_manager() explicitly return None on exception
    
    pre-commit
    
    update docstrings on BasePersona
    
    pre-commit
    
    add return type annotation to forward_reply_stream()
    
    remove unused return
    
    add comment explaining _init_persona_classes()
    
    add docstrings to persona_manager methods
    
    remove redundant error logs
    
    pre-commit
    
    fix mypy errors
    
    remove tests on chat handlers as they are superseded by personas
    
    fix type annotation for Py39 compat
    
    rename forward_reply_stream() to stream_message()
    
    add send_message() method to BasePersona
    
    pre-commit

---
+++ packages/jupyter-ai-test/jupyter_ai_test/debug_persona.py
@@ -0,0 +1,24 @@
+from jupyter_ai.personas.base_persona import BasePersona, PersonaDefaults
+from jupyterlab_chat.models import Message, NewMessage
+
+
+class DebugPersona(BasePersona):
+    """
+    The Jupyternaut persona, the main persona provided by Jupyter AI.
+    """
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    @property
+    def defaults(self):
+        return PersonaDefaults(
+            name="DebugPersona",
+            avatar_path="/api/ai/static/jupyternaut.svg",
+            description="A mock persona used for debugging in local dev environments.",
+            system_prompt="...",
+        )
+
+    async def process_message(self, message: Message):
+        self.ychat.add_message(NewMessage(body="Hello!", sender=self.id))
+        return
