144a087ec47738752ff4a9174decd868e4d82f0d
Lumberbot (aka Jack) <39504233+meeseeksmachine@users.noreply.github.com>
2025-05-20 14:46:23 -0700

    Backport PR #1333: Added a local identity provider. (#1334)
    Co-authored-by: Piyush Jain <piyushjain@duck.com>

---
+++ packages/jupyter-ai/jupyter_ai/auth/identity.py
@@ -0,0 +1,39 @@
+import asyncio
+import getpass
+
+from jupyter_server.auth.identity import IdentityProvider, User
+
+
+def create_initials(username):
+    """Creates initials combining first 2 consonants"""
+
+    username = username.lower()
+
+    # Default: return first two unique consonants
+    consonants = [c for c in username if c in "bcdfghjklmnpqrstvwxyz"]
+    if len(consonants) >= 2:
+        return (consonants[0] + consonants[1]).upper()
+
+    # Fallback: first two characters
+    return username[:2].upper()
+
+
+class LocalIdentityProvider(IdentityProvider):
+    """IdentityProvider that determines username from system user."""
+
+    def get_user(self, handler):
+        try:
+            username = getpass.getuser()
+            user = User(
+                username=username,
+                name=username,
+                initials=create_initials(username),
+                color=None,
+            )
+            return user
+        except OSError as e:
+            self.log.debug(
+                "Could not determine username from system. Falling back to anonymous"
+                f"user."
+            )
+            return self._get_user(handler)
