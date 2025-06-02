f79b8c48f033bcbd872a5ed85f1607c06b82898f
Nicolas Brichet <nicolas.brichet@quantstack.net>
2025-06-02 14:07:13 +0200

    Periodically update the personna awareness state to keep it alive

--- packages/jupyter-ai/jupyter_ai/personas/persona_awareness.py
+++ packages/jupyter-ai/jupyter_ai/personas/persona_awareness.py
@@ -1,9 +1,12 @@
+from asyncio import Task, TaskGroup, create_task
 import random
 from contextlib import contextmanager
 from dataclasses import asdict
 from logging import Logger
+from time import time
 from typing import TYPE_CHECKING, Any, Optional
 
+from anyio import create_task_group, sleep
 from jupyterlab_chat.models import User
 from jupyterlab_chat.ychat import YChat
 from pycrdt import Awareness
@@ -38,6 +41,7 @@ class PersonaAwareness:
 
     _original_client_id: int
     _custom_client_id: int
+    _heartbeat: Task | None
 
     def __init__(self, *, ychat: YChat, log: Logger, user: Optional[User]):
         # Bind instance attributes
@@ -60,6 +64,8 @@ class PersonaAwareness:
         if self.user:
             self._register_user()
 
+        self._heartbeat = create_task(self._start())
+
     @contextmanager
     def as_custom_client(self) -> "Iterator[None]":
         """
@@ -76,6 +82,32 @@ class PersonaAwareness:
             yield
         finally:
             self.awareness.client_id = self._original_client_id
+
+    @property
+    def outdated_timeout(self) -> int:
+        """
+        Returns the outdated timeout of the document awareness, in milliseconds.
+        """
+        return self.awareness._outdated_timeout
+
+    def _get_time(self) -> int:
+        return int(time() * 1000)
+
+    async def _start(self) -> None:
+        """
+        Starts the awareness heartbeat task, which periodically renews the state.
+        """
+        while True:
+            await sleep(self.outdated_timeout / 1000 / 10)
+            now = self._get_time()
+            local_state = self.get_local_state()
+            if (
+                local_state is not None
+                and self.outdated_timeout / 2 <= now - self.awareness.meta[self._custom_client_id]["lastUpdated"]
+            ):
+                # renew local clock
+                with self.as_custom_client():
+                    self.awareness.set_local_state(local_state)
 
     def _register_user(self):
         if not self.user:
