56d88b75fd04b461efb43e5dfef9c6377f9f8461
David L. Qiu <david@qiu.dev>
2025-06-09 14:14:50 -0700

    Remove unused code from v3 `main` branch (#1369)
    * remove slash commands & context commands
    
    * remove dask & faiss-cpu as dependencies are now unused
    
    * remove now-unused help message template
    
    * remove unused directory test
    
    * remove unused constants.py
    
    * remove unused CSS
    
    * pre-commit

--- packages/jupyter-ai/jupyter_ai/callback_handlers/__init__.py
+++
@@ -1,6 +0,0 @@
-"""
-Provides classes which extend `langchain_core.callbacks:BaseCallbackHandler`.
-Not to be confused with Jupyter AI chat handlers.
-"""
-
-from .metadata import MetadataCallbackHandler
--- packages/jupyter-ai/jupyter_ai/chat_handlers/__init__.py
+++
@@ -1,6 +0,0 @@
-from .ask import AskChatHandler
-from .base import BaseChatHandler, SlashCommandRoutingType
-from .default import DefaultChatHandler
-from .generate import GenerateChatHandler
-from .help import HelpChatHandler
-from .learn import LearnChatHandler
