02a823d338e94c5368363fd15643fa4913d9f902
haofan <haofan.zhang21@gmail.com>
2025-06-24 13:47:58 -0700

    Refresh the list of supported Gemini models. (#1381)

--- packages/jupyter-ai-magics/jupyter_ai_magics/partner_providers/gemini.py
+++ packages/jupyter-ai-magics/jupyter_ai_magics/partner_providers/gemini.py
@@ -7,14 +7,11 @@ class GeminiProvider(BaseProvider, GoogleGenerativeAI):
     id = "gemini"
     name = "Gemini"
     models = [
+        "gemini-2.5-pro",
+        "gemini-2.5-flash",
+        "gemini-2.0-flash-lite",
         "gemini-1.5-pro",
         "gemini-1.5-flash",
-        "gemini-1.0-pro",
-        "gemini-1.0-pro-001",
-        "gemini-1.0-pro-latest",
-        "gemini-1.0-pro-vision-latest",
-        "gemini-pro",
-        "gemini-pro-vision",
     ]
     model_id_key = "model"
     auth_strategy = EnvAuthStrategy(name="GOOGLE_API_KEY")
