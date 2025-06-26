18ae39fff306b9ebce30206b049ad92f70f50e12
David L. Qiu <david@qiu.dev>
2025-06-26 16:55:16 -0700

    set up `uv` in CI using the official GH action

--- scripts/ci-setup.sh
+++ scripts/ci-setup.sh
@@ -2,15 +2,16 @@
 #
 # Setup script used to initialize CI environments in GitHub workflows.
 # Not intended for human use.
+#
+# NOTE: this script requires the `astral-sh/setup-uv` GitHub action to run
+# before being called in a GitHub workflow. See this page for more guidance on
+# running `uv` inside a GitHub workflow:
+# https://docs.astral.sh/uv/guides/integration/github/
 
 set -eux
 
-# Install uv
-# conda is installed from base-setup action
-conda install uv --channel conda-forge
-
 # Install JupyterLab
-pip install "jupyterlab>=4.4"
+uv pip install "jupyterlab>=4.4"
 
 # Build & install packages
 jlpm install
