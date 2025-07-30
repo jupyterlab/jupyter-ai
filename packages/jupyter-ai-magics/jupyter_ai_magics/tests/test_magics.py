f8fbda68121dd5d1c29e9ca40bdc805bd3ba3ecc
Sanjiv Das <srdas@scu.edu>
2025-07-30 09:58:38 -0700

    Update test_magics.py

--- packages/jupyter-ai-magics/jupyter_ai_magics/tests/test_magics.py
+++ packages/jupyter-ai-magics/jupyter_ai_magics/tests/test_magics.py
@@ -14,7 +14,7 @@ def ip() -> InteractiveShell:
 
 
 def test_aliases_config(ip):
-    ip.config.AiMagics.aliases = {"my_custom_alias": "my_provider:my_model"}
+    ip.config.AiMagics.initial_aliases = {"my_custom_alias": "my_provider:my_model"}
     ip.extension_manager.load_extension("jupyter_ai_magics")
     providers_list = ip.run_line_magic("ai", "list").text
     assert "my_custom_alias" in providers_list
